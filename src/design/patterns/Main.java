da478d5f428c8ab7700533bb5bd7fc447e85bef1
Matt Rudin <matthias.rudin@live.com>
2019-06-17 19:48:21 +0200

    Add adapter pattern

--- src/design/patterns/Main.java
+++ src/design/patterns/Main.java
@@ -1,24 +1,17 @@
 package design.patterns;
 
-import design.patterns.command.Light;
-import design.patterns.command.LightOffCommand;
-import design.patterns.command.LightOnCommand;
-import design.patterns.command.RemoteControl;
+import design.patterns.adapter.EnumerationIterator;
+
+import java.util.Enumeration;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) {
-        RemoteControl remote = new RemoteControl();
-        Light light = new Light();
-        LightOnCommand lightOn = new LightOnCommand(light);
-        LightOffCommand lightOff = new LightOffCommand(light);
-
-        // Set lightOn command
-        remote.setSlot(lightOn);
-        remote.pressButton();
-
-        // Set lightOff command
-        remote.setSlot(lightOff);
-        remote.pressButton();
+        Enumeration enumeration = new StringTokenizer("This is a string.");
+        EnumerationIterator enumAdapter = new EnumerationIterator(enumeration);
+        for (enumAdapter.next(); enumAdapter.hasNext();) {
+            System.out.println(enumAdapter.next());
+        }
     }
 }
